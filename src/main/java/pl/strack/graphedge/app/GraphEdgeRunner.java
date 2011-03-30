145615595667312db531ac1b7b5b0fe2f6571809
Konrad Strack <konrad@strack.pl>
2011-03-31 00:45:17 +0200

    reading graph from file

--- src/main/java/pl/strack/graphedge/app/GraphEdgeRunner.java
+++ src/main/java/pl/strack/graphedge/app/GraphEdgeRunner.java
@@ -5,10 +5,18 @@ import java.io.FileNotFoundException;
 import pl.strack.graphedge.builder.FileGraphBuilder;
 import pl.strack.graphedge.core.Graph;
 
+import com.google.inject.Inject;
+
 public class GraphEdgeRunner {
+	
+	private final FileGraphBuilder builder;
+	
+	@Inject
+	public GraphEdgeRunner(FileGraphBuilder builder) {
+		this.builder = builder;
+	}
 
 	public void run() {
-		FileGraphBuilder builder = new FileGraphBuilder();
 		try {
 			Graph graph = builder.build("src/main/resources/graphs/simple1.g");
 		} catch (FileNotFoundException e) {
