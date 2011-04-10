14559c0114e8f10f748228ac771991c73ca297ca
Konrad Strack <konrad@strack.pl>
2011-04-10 15:16:57 +0200

    switch statement repaired

--- src/main/java/pl/strack/graphedge/painter/GraphEdgePainter.java
+++ src/main/java/pl/strack/graphedge/painter/GraphEdgePainter.java
@@ -5,33 +5,40 @@ import pl.strack.graphedge.classifier.GraphType;
 import pl.strack.graphedge.core.Graph;
 
 public class GraphEdgePainter {
-	
+
 	private GraphClassifier classifier;
-	
+
 	public GraphEdgePainter() {
 		classifier = new GraphClassifier();
 	}
-	
+
 	private void paintSimpleGraph(Graph graph) {
-		
+
 	}
-	
+
 	private void paintBipartiteGraph(Graph graph) {
-		
+
 	}
-	
+
 	private void paintTree(Graph graph) {
-		
+
 	}
-	
+
 	public void paintGraph(Graph graph) {
 		GraphType type = classifier.determineGraphType(graph);
-		
-		switch(type) {
-			case TREE: 			paintTree(graph);
-			case BIPARTITE:  	paintBipartiteGraph(graph);
-			case SIMPLE:		paintSimpleGraph(graph);
+
+		switch (type) {
+		case TREE:
+			paintTree(graph);
+			break;
+		case BIPARTITE:
+			paintBipartiteGraph(graph);
+			break;
+		case SIMPLE:
+		default:
+			paintSimpleGraph(graph);
+			break;
 		}
 	}
-	
+
 }
