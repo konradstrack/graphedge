7f8aa8f12d8a62ded0d273fa3ba8afb5c88884a0
Szymon Mentel <mentel.szymon@gmail.com>
2011-04-12 23:35:01 +0200

    Painting Simple Graph added.

--- src/main/java/pl/strack/graphedge/painter/GraphEdgePainter.java
+++ src/main/java/pl/strack/graphedge/painter/GraphEdgePainter.java
@@ -1,11 +1,13 @@
 package pl.strack.graphedge.painter;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import pl.strack.graphedge.classifier.GraphClassifier;
 import pl.strack.graphedge.classifier.GraphType;
 import pl.strack.graphedge.core.Edge;
 import pl.strack.graphedge.core.Graph;
+import pl.strack.graphedge.painter.exceptions.GraphColoringNotFoundException;
 
 import com.google.inject.Inject;
 
@@ -27,7 +29,7 @@ public class GraphEdgePainter {
 		Set<Integer> vertices = graph.vertexSet();
 		Integer vertex = vertices.iterator().next();
 		for (Integer v : vertices) {
-			if(graph.degreeOf(v) > graph.degreeOf(vertex)) {
+			if (graph.degreeOf(v) > graph.degreeOf(vertex)) {
 				vertex = v;
 			}
 		}
@@ -35,62 +37,101 @@ public class GraphEdgePainter {
 		return vertex;
 	}
 
+	private boolean paintSimpleGraph(Graph graph, Iterator<Edge> edgesIt, int maxVertexDegree) {
+		
+		if(! edgesIt.hasNext()) {
+			
+			if(checkColoring(graph)) {
+				return true;
+			} else {
+				return false;
+			}
+			
+		}
+		
+		Edge edge = edgesIt.next();
+		for(int col = 1; col <= maxVertexDegree; ++col) {
+			
+			edge.setColor(col); 
+			if( paintSimpleGraph(graph, edgesIt, maxVertexDegree) ) {
+				return true;
+			}
+			
+		}
+		
+		return false;
+		
+	}
+
 	private int paintSimpleGraph(Graph graph) {
-		return 0;
+
+		Iterator<Edge> edgesIt = graph.edgeSet().iterator();
+		int maxVertexDegree = getMaxDegreeVertex(graph);
+
+		if (paintSimpleGraph(graph, edgesIt, maxVertexDegree)) {
+			return maxVertexDegree;
+		}
+		if (paintSimpleGraph(graph, edgesIt, maxVertexDegree + 1)) {
+			return maxVertexDegree + 1;
+		}
+
+		throw new GraphColoringNotFoundException();
+
 	}
 
 	private int paintBipartiteGraph(Graph graph) {
 		return 0;
 	}
 
-	private void paintTree(Graph graph, Integer vertex, VertexStateMark[] vertexState, int parentEdgeColor) {
+	private void paintTree(Graph graph, Integer vertex,
+			VertexStateMark[] vertexState, int parentEdgeColor) {
 
-		vertexState[vertex.intValue() -1] = VertexStateMark.GREY;
-		
+		vertexState[vertex.intValue() - 1] = VertexStateMark.GREY;
+
 		Set<Edge> edges = graph.edgesOf(vertex);
 		Integer nextVertex;
 		int lastColor = 1;
 		for (Edge e : edges) {
-			
+
 			if (graph.getEdgeSource(e) == vertex) {
 				nextVertex = graph.getEdgeTarget(e);
 			} else {
 				nextVertex = graph.getEdgeSource(e);
 			}
 
-			if(vertexState[nextVertex.intValue() - 1] == VertexStateMark.WHITE) {
-				
-				if(lastColor == parentEdgeColor) {
+			if (vertexState[nextVertex.intValue() - 1] == VertexStateMark.WHITE) {
+
+				if (lastColor == parentEdgeColor) {
 					e.setColor(++lastColor);
 				} else {
 					e.setColor(lastColor);
 				}
-				
+
 				paintTree(graph, nextVertex, vertexState, lastColor);
 				++lastColor;
-		
+
 			}
-					
+
 		}
-		
+
 		vertexState[vertex.intValue() - 1] = VertexStateMark.BLACK;
 
 	}
 
 	private int paintTree(Graph graph) {
-		
+
 		int verticesNumber = graph.vertexSet().size();
 		VertexStateMark[] vertexState = new VertexStateMark[verticesNumber];
-		for(int i = 0; i < verticesNumber; ++i) {
+		for (int i = 0; i < verticesNumber; ++i) {
 			vertexState[i] = VertexStateMark.WHITE;
 		}
-		
+
 		Integer vertex = graph.vertexSet().iterator().next();
 		paintTree(graph, vertex, vertexState, 0);
-		
-		//tree is a bipartite graph, so: chromatic index == max vertex degree
-		return graph.degreeOf( getMaxDegreeVertex(graph) );
-		
+
+		// tree is a bipartite graph, so: chromatic index == max vertex degree
+		return graph.degreeOf(getMaxDegreeVertex(graph));
+
 	}
 
 	public int paintGraph(Graph graph) {
@@ -112,29 +153,30 @@ public class GraphEdgePainter {
 
 		return colors;
 	}
-	
+
 	public boolean checkColoring(Graph graph) {
-		
-		boolean[] isColorUsed = new boolean[graph.degreeOf( getMaxDegreeVertex(graph) ) + 1];
-				
-		for(Integer v : graph.vertexSet()) {
-			
-			for(int i = 0; i < isColorUsed.length; ++i) {
+
+		boolean[] isColorUsed = new boolean[graph
+				.degreeOf(getMaxDegreeVertex(graph)) + 1];
+
+		for (Integer v : graph.vertexSet()) {
+
+			for (int i = 0; i < isColorUsed.length; ++i) {
 				isColorUsed[i] = false;
 			}
 
-			for(Edge e : graph.edgesOf(v)) {
-				
-				if( isColorUsed[e.getColor() - 1] ) {
+			for (Edge e : graph.edgesOf(v)) {
+
+				if (isColorUsed[e.getColor() - 1]) {
 					return false;
 				} else {
 					isColorUsed[e.getColor() - 1] = true;
 				}
-				
+
 			}
-			
+
 		}
-		
+
 		return true;
 	}
 
