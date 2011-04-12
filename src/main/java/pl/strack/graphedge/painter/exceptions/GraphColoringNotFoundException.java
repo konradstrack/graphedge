d6ca644fc613f43f6cf00cd48e1264bfeca82fcf
Szymon Mentel <mentel.szymon@gmail.com>
2011-04-12 23:36:31 +0200

    Painting Simple Graph added.

---
+++ src/main/java/pl/strack/graphedge/painter/exceptions/GraphColoringNotFoundException.java
@@ -0,0 +1,10 @@
+package pl.strack.graphedge.painter.exceptions;
+
+public class GraphColoringNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5883872259621017290L;
+
+}
